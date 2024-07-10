35b23a0da960ccfefcb7804171ec72406a39e729
valitovgaziz <a-c-v-a-l@mail.ru>
2024-07-10 12:12:38 +0500

    env var initializers

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
 import (
-	p "fmt"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/valitovgaziz/rest-api/initializers"
 )
 
+var (
+	server *gin.Engine
+)
+
+func init() {
+	if config, err := initializers.LoadConfig("."); err!= nil {
+		log.Fatal("Can't load environment variables", err)
+	}
+
+	initializers.ConnectDB(&config)
+
+	server = gin.Default()
+}
+
 func main() {
-	p.Println("You are should recomend me")
+	router := server.Group("/api")
+	router.GET("/hello", func(c *gin.Context) {
+		message := "from gin"
+		c.JSON(http.StatusOK, gin.H{"status": "succes", "message": message})
+	})
+
+	log.Fatal(server.)
 }
