1c065ebf16bb5bf6ffc0bda4e6f1300a5c22afb1
valitovgaziz <a-c-v-a-l@mail.ru>
2024-07-11 16:10:39 +0500

    save  insert without tests

--- models/Client.go
+++ models/Client.go
@@ -5,8 +5,8 @@ import "gorm.io/gorm"
 type Client struct {
 	gorm.Model    `gorm:"embedded"`
 	ID            uint   `gorm:"column:id;autoincrement;primaryKey"`
-	Client_name   string `gorm:"column:client_name;not null;type:varchar(255)"`
+	Client_name   string `gorm:"column:client_name;not null;type:varchar(255);unique"`
 	Exchange_name string `gorm:"column:exchange_nam;not nulltype:varchar(255)"`
-	Label         string `gorm:type:varchar(255)"`
+	Label         string `gorm:"type:varchar(255)"`
 	Pair          string `gorm:"type:varchar(255)"`
 }
---
+++ services/OrderBook.go
@@ -0,0 +1,57 @@
+package services
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/valitovgaziz/rest-api/initializers"
+	"github.com/valitovgaziz/rest-api/models"
+)
+
+func GetOrderBook(exchange_name string, pair string) ([]*models.OrderBook, error) {
+	var orderBooks []*models.OrderBook
+
+	// Используем метод Find для поиска записей в базе данных
+	if err := initializers.DB.Where("exchange = ? AND pair = ?", exchange_name, pair).Find(&orderBooks).Error; err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("no order book found for %s/%s", exchange_name, pair)
+		}
+		return nil, err
+	}
+
+	return orderBooks, nil
+}
+
+func GetOrder(client *models.Client) ([]*models.HistoryOrder, error) {
+	var historyOrders []*models.HistoryOrder
+
+	// Используем метод Find для поиска записей в базе данных
+	if err := initializers.DB.Where("client_name = ?", client.Client_name).Find(&historyOrders).Error; err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("no orders found for client")
+		}
+		return nil, err
+	}
+
+	return historyOrders, nil
+}
+
+func SaveOrder(client *models.Client, order *models.HistoryOrder) error {
+	// Создаем транзакцию для безопасного выполнения операций
+	tx := initializers.DB.Begin()
+
+	// Добавляем новый заказ
+	if err := tx.Create(order).Error; err != nil {
+		tx.Rollback() // В случае ошибки откатываем транзакцию
+		return err
+	}
+
+	// Обновляем клиента, добавляя новый заказ в историю
+	if err := tx.Model(&client).Association("HistoryOrders").Append(order); err != nil {
+		tx.Rollback() // В случае ошибки откатываем транзакцию
+		return err
+	}
+
+	tx.Commit() // В случае ошибки откатываем транзакцию
+	return nil
+}
